8cdd01cd69dbf0443097ac5180e7328248150872
Ashunar0 <asahi.gaia1530@gmail.com>
2026-06-03 23:37:18 +0900

    chore(web): enable react compiler

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,13 +3,21 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+const ReactCompilerConfig = {
+  target: "19",
+};
+
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
     tailwindcss(),
   ],
 });
